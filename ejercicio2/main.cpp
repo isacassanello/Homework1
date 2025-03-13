87f9f8659664826346b2924403a178ede82ae251
Isabella Cassanello <icassanello@udesa.edu.ar>
2025-03-13 10:15:58 -0300

    nueva funcion hecha para que el usuario ingrese los mensajes, nombre de usuario, etc

--- ejercicio2/main.cpp
+++ ejercicio2/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 using namespace std;
 
-enum nivelSeguridad {DEBUG, INFO, WARNING, ERROR, CRITICAL, SECURITY};
+enum nivelSeguridad {DEBUG = 1, INFO, WARNING, ERROR, CRITICAL, SECURITY};
 
 // punto a
 void logMessage(string msg, nivelSeguridad nivel){
@@ -52,18 +52,61 @@ void logMessage(string msg_de_acceso, string usuario){
     archivo_log.close();
 }
 
+void ingresarMensajes(){
+    int opcion; 
+    string msg, archivo, usuario;
+    int linea;
+
+    while (true){
+        cout << "Seleccione un nivel de seguridad (el numero):\n    1) DEBUG\n    2) INFO\n    3) WARNING\n    4) ERROR\n    5) CRITICAL\n    6) SECURITY\n    7) Salir\n";
+        cout << "Opcion: ";
+        cin >> opcion;
+        cin.ignore();
+
+        if (opcion == 7){
+            cout << "Saliendo del programa." << endl; break;
+        }
+
+        if (opcion < 1 || opcion > 6){
+            cout << "Nivel de seguridad inexistente. Ingrese nuevamente\n";
+            continue;
+        }
+
+        if (opcion == 4){
+            cout << "Indique el mensaje de error: ";
+            getline(cin, msg);
+
+            cout << "Indique en qué archivo donde se encuentra el error: ";
+            getline(cin, archivo);
+
+            cout << "Indique la linea donde se encuentra el error dentro del archivo: ";
+            cin >> linea;
+            cin.ignore();
+
+            logMessage(msg, archivo, linea);
+        
+        } else if (opcion == 6){
+            cout << "Ingrese su nombre de usuario: ";
+            getline(cin, usuario);
+
+            cout << "Ingrese el mensaje de acceso: ";
+            getline(cin, msg);
+
+            logMessage(msg, usuario);
+        
+        } else {
+            cout << "Ingrese el mensaje: ";
+            getline(cin, msg);
+
+            logMessage(msg, static_cast<nivelSeguridad>(opcion));
+        }
+    }
+}
+
 
 
 int main() {
-    // Pruebas de las diferentes funcionalidades de log
-    logMessage("Inicializando sistema...", INFO);
-    logMessage("Conectando a la base de datos...", DEBUG);
-    logMessage("Advertencia: Uso de memoria alto", WARNING);
-    logMessage("Error en módulo de autenticación", "auth.cpp", 45);
-    logMessage("Acceso permitido", "usuario123", SECURITY);
-    logMessage("Acceso denegado", "usuario456", SECURITY);
- 
-
+    ingresarMensajes();
     cout << "Mensajes de log registrados en 'log.txt'" << endl;
     return 0;
 }
