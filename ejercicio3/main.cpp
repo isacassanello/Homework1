ac99c24b78dbd55da95742f6e2285a4caa77a2ec
Isabella Cassanello <icassanello@udesa.edu.ar>
2025-03-20 18:30:52 -0300

    EJERCICIO 3 CON COMENTARIOS

--- ejercicio3/main.cpp
+++ ejercicio3/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-#include <memory>
+#include <memory> // libreria para usar smart pointers
 using namespace std;
 
 struct node{
     shared_ptr<node> next;
-    int value;
+    int value; 
 };
 
 // i. create_node()
 shared_ptr<node> create_node(int value){
-    return make_shared<node>(node{nullptr, value});
+    return make_shared<node>(node{nullptr, value}); // uso make_shared<node>() para crear un nodo en memoria dinamica y devolver un shared_ptr
+    // inicializo el nodo con node->next = nullptr y node->value = value
 }
 
 // ii. push_front():
@@ -23,14 +24,14 @@ void push_front(shared_ptr<node>& head, int value){
 void push_back(shared_ptr<node>& head, int value){
     auto new_node = create_node(value);
     
-    if (!head) {
+    if (!head) { // caso lista vacia
         head = new_node;
         return;
     }
 
-    auto temp = head;
-    while (temp->next != NULL){
-        temp = temp->next;
+    auto temp = head; // nodo auxiliar para recorrer la lista
+    while (temp->next != NULL){ 
+        temp = temp->next; // recorro toda la lista hasta llegar al ultimo nodo
     }
     temp->next = new_node;
 }
@@ -39,35 +40,36 @@ void push_back(shared_ptr<node>& head, int value){
 void insert(shared_ptr<node>& head, int value, int position){
     auto new_node = create_node(value);
 
-    if (position <= 1 || !head){
-        push_front(head, value);
+    if (position <= 1 || !head){ // si la posición es 1 (o un numero menor) o la lista está vacía, insertar al frente utilizando push_front()
+        push_front(head, value); 
         return;
     }
 
-    auto temp = head;
-    int count = 1;
-    while (temp->next != NULL && count < position - 1){
+    auto temp = head; // nodo auxiliar para recorrer la lista
+    int count = 1; // contador para determinar cuantas posiciones estoy avanzando en la lista
+    while (temp->next != NULL && count < position - 1){ // avanzar hasta la posición deseada
         temp = temp->next;
         count++;
     }
 
-    if (temp->next == NULL && count < position - 1){
+    if (temp->next == NULL && count < position - 1){ // caso: posicion es mas larga que la lista
         cout << "\nLa posicion " << position << " es mayor que el largo de la lista. Se va a insertar al final\n";
     }
     
-    new_node->next = temp->next;
-    temp->next = new_node;
+    new_node->next = temp->next; // conecto el nuevo nodo con el siguiente nodo
+    temp->next = new_node; // inserto el nuevo nodo en la posición correcta
 }
 
 // v. erase(): 
 void erase(shared_ptr<node>& head, int position){
-    if (head == NULL) return;
+    if (head == NULL) return; // si la lista esta vacia, no hago nada
 
-    if (position <= 1){
+    if (position <= 1){ // caso: quiero eliminar la cabeza de la lista
         head = head->next;
         return;
     }
 
+    // misma logica que en la funcion insert()
     auto temp = head;
     int count = 1;
 
@@ -76,13 +78,13 @@ void erase(shared_ptr<node>& head, int position){
         count++;
     }
 
-    if (head->next->next == NULL){
+    if (head->next == NULL){ // caso: la posicion es mayor al tamaño de la lista
         cout << "\nLa posicion " << position << " es mayor que el largo de la lista. Se elimia el ultimo nodo\n";
-        temp->next = nullptr; 
+        temp->next = nullptr; // elimino el ultimo nodo
         return;
     }
 
-    temp->next = temp->next->next;
+    temp->next = temp->next->next; 
 }
 
 
@@ -106,28 +108,49 @@ void print_list(shared_ptr<node>& head){
 }
 
 int main(){
-    shared_ptr<node> head = nullptr;
+    shared_ptr<node> head = nullptr; // inicializo la lista vacia
     
-    push_front(head, 10);
+    // pruebo todas las funciones
     push_front(head, 5);
+    insert(head, 12, 2);
+    print_list(head); // esperado: 5 -> 12 -> NULL
+
+    erase(head, 2);
+    print_list(head); // esperado: 5 -> NULL
+
+    push_back(head, 6);
     push_front(head, 1);
-    insert(head, 15, 3);
-    print_list(head); // esperado: 1 -> 5 -> 15 -> 10 -> NULL
+    insert(head, 3, 2);
+    print_list(head); // esperado: 1 -> 3 -> 5 -> 6 -> NULL
 
     push_back(head, 20);
     push_back(head, 30);
-    push_back(head, 40);
     insert(head, 34, 76);
-    print_list(head); // esperado: 1 -> 5 -> 15 -> 10 -> 20 -> 30 -> 40 -> 34 -> NULL
+    push_back(head, 40);
+    print_list(head); // esperado: 1 -> 3 -> 5 -> 6 -> 20 -> 30 -> 34 -> 40 -> NULL
 
-    erase(head, 1);
-    print_list(head); // esperado: 5 -> 15 -> 10 -> 20 -> 30 -> 40 -> 34 -> NULL
+    erase(head, 0);
+    push_front(head, 2);
+    print_list(head); // esperado: 2 -> 3 -> 5 -> 6 -> 20 -> 30 -> 34 -> 40 -> NULL
 
-    erase(head, 5);
-    print_list(head); // esperado: 5 -> 15 -> 10 -> 20 -> 40 -> 34 -> NULL
+    insert(head, 1, -2);
+    print_list(head); // esperado: 1 -> 2 -> 3 -> 5 -> 6 -> 20 -> 30 -> 34 -> 40 -> NULL
+
 
     erase(head, 100);
-    print_list(head); // esperado: 5 -> 15 -> 10 -> 20 -> 40 -> NULL
+    print_list(head); // esperado: 1 -> 2 -> 3 -> 5 -> 6 -> 20 -> 30 -> 34 -> NULL
+
+    erase(head, 5);
+    erase(head, 7);
+    erase(head, 4);
+    erase(head, 2);
+    erase(head, 6);
+    erase(head, 1);
+    erase(head, 3);
+    print_list(head); // esperado: 3 -> NULL
+
+    erase(head, 0);
+    print_list(head); // esperado: [Lista vacía]
 
     return 0;
 }
